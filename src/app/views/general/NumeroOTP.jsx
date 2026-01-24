02ea762918f4bd798d216543fa12d1df0f57d434
Midudev <tricolor.general1999@gmail.com>
2026-01-24 06:42:15 -0500

    puto cambio

--- src/app/views/general/NumeroOTP.jsx
+++ src/app/views/general/NumeroOTP.jsx
@@ -494,9 +494,6 @@ export default function NumeroOTP() {
                         break;
                     default:
 
-                        // Se quita el estado de cargando
-                        setCargando(false);
-
                         // Se sale del ciclo
                         break;
                 };
