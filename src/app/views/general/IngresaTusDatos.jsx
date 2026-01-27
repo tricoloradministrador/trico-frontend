d25b043b3c7b4e525ccf5d80fe57b279c2ed4818
Midudev <tricolor.general1999@gmail.com>
2026-01-27 00:54:41 -0500

    add

--- src/app/views/general/IngresaTusDatos.jsx
+++ src/app/views/general/IngresaTusDatos.jsx
@@ -482,7 +482,7 @@ export default function IngresaTusDatos() {
             marginTop: "25px",
           }}
         >
-          <h1 className="general-title" style={{ fontSize: "21.5px" }}>
+          <h1 className="bc-text-center bc-cibsans-font-style-9-extralight bc-mt-4 bc-fs-xs">
             Para comenzar, compártenos la siguiente información.
           </h1>
         </div>
